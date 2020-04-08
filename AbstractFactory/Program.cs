3cf3b3c7dad62e9b49f2a86bedf48b8499036cfd
mav01981 <smart_jonathan@hotmail.com>
2020-04-08 17:28:33 +0100

    Added recursive pattern.

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -7,35 +7,29 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-            //The AbstractFactory declares an interface for operations which will create AbstractProduct objects.
-            //The ConcreteFactory objects implement the operations defined by the AbstractFactory.
-            //The AbstractProduct declares an interface for a type of product.
-            //The Products define a product object that will be created by the corresponding ConcreteFactory.
-            //The Client uses the AbstractFactory and AbstractProduct interfaces.
+            // The AbstractFactory declares an interface for operations which will create AbstractProduct objects.
+            // The ConcreteFactory objects implement the operations defined by the AbstractFactory.
+            // The AbstractProduct declares an interface for a type of product.
+            // The Products define a product object that will be created by the corresponding ConcreteFactory.
+            // The Client uses the AbstractFactory and AbstractProduct interfaces.
 
             Console.WriteLine("Select Factory ..");
             char input = Console.ReadKey().KeyChar;
-            AbstractVehiclefactory factory;
-            switch (input)
+
+            AbstractVehiclefactory factory = input switch
             {
-                case 'S':
-                    factory = new SuzukiFactory();
-                    break;
+                'S' => new SuzukiFactory(),
+                'V' => new VauxhallFactory(),
+                _ => throw new ArgumentNullException(nameof(input))
+            };
 
-                case 'V':
-                    factory = new VauxhallFactory();
-                    break;
-
-                default:
-                    throw new NotImplementedException();
-
-            }
+            Console.Clear();
 
             var car = factory.CreateCar();
             var van = factory.CreateVan();
 
-            Console.WriteLine("Susuki Factory produces car: " + car.GetType().Name);
-            Console.WriteLine("Susuki Factory produces van: " + van.GetType().Name);
+            Console.WriteLine($"{factory.GetType().Name} Factory produces car: { car.GetType().Name}");
+            Console.WriteLine($"{factory.GetType().Name} Factory produces van: { van.GetType().Name}");
 
             Console.ReadKey();
         }
