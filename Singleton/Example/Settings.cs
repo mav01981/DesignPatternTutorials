26f4f79f8b5907020f8a3a6452e1dc54e09ac0d1
mav01981 <smart_jonathan@hotmail.com>
2020-08-20 11:56:51 +0100

    Added Thread safe Singleton Example.

---
+++ Singleton/Example/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Singleton.Example
+{
+    public class Settings
+    {
+        public string DatabaseConnectionString { get; set; } = "";
+        public string ProcessFolder { get; set; }
+        public string SaveFolder { get; set; }
+
+        public override string ToString() =>
+           new StringBuilder()
+           .AppendLine(DatabaseConnectionString)
+           .AppendLine(ProcessFolder)
+           .AppendLine(SaveFolder)
+           .ToString();
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using Singleton.Example;
+using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton(logger, new Settings
+                {
+                    DatabaseConnectionString = "Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;",
+                    ProcessFolder = "c:/processFolder",
+                    SaveFolder = "c:/saveFolder"
+                });
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton(logger, new Settings
+                {
+                    DatabaseConnectionString = "Server=myServerAddress2;Database=myDataBase2;Trusted_Connection=True;",
+                    ProcessFolder = "c:/processFolder2",
+                    SaveFolder = "c:/saveFolder2"
+                });
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+
+            Console.Read();
+
+            //Output.
+            //Server = myServerAddress; Database = myDataBase; Trusted_Connection = True;
+            //c:/ processFolder
+            //c:/ saveFolder
+
+            //Server = myServerAddress; Database = myDataBase; Trusted_Connection = True;
+            //c:/ processFolder
+            //c:/ saveFolder
+        }
+
+        private static void TestSingleton(ILogger logger, Settings value)
+        {
+            Singleton singleton = Singleton.GetInstance(value);
+
+            logger.LogInformation($"{singleton.Value}");
+        }
+    }
+}
